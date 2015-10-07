50beb5055021b6f6447392258e287b2304f0028f
shumod <absborodin@yandex.ru>
2015-10-07 10:05:49 +0300

    First commit
    first

---
+++ templates/event.tpl.php
@@ -0,0 +1,2 @@
+<h1 class="title"><?php print $title;?></h1>
+<div class="place-organization">Организация: <?php print $organization;?></div>
---
+++ templates/organization.tpl.php
@@ -0,0 +1,1 @@
+<h1 class="title"><?php print $title;?></h1>

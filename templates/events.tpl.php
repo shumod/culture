67c3dd75d73c7c82b05dc579d027c677c56634c7
shumod <absborodin@yandex.ru>
2015-10-13 14:42:52 +0300

    Рефакторинг кода

--- templates/events.tpl.php
+++ templates/events.tpl.php
@@ -3,4 +3,5 @@
 	<div class="title"><?php print $title;?></div>
 	<div class="description"><?php print $description;?></div>
 	<div class="price"><?php print $price;?></div>
+	<div class="seance"><?php print $seance;?></div>	
 </div>

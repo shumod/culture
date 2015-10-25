707d105369c1aa434523ce3903bc88f6728226fd
shumod <absborodin@ya.ru>
2015-10-25 21:22:16 +0300

    Мелкие правки

--- templates/events.tpl.php
+++ templates/events.tpl.php
@@ -1,7 +1,7 @@
 <div class="event">
 	<div class="image"><?php print $image;?></div>	
+	<div class="seance"><?php print $seance;?></div>
 	<div class="title"><?php print $title;?></div>
 	<div class="description"><?php print $description;?></div>
-	<div class="price"><?php print $price;?></div>
-	<div class="seance"><?php print $seance;?></div>	
+	<div class="price"><?php print $price;?></div>	
 </div>

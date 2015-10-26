9b7dd2ff2bb4aa848537b4f610cef68f07252f78
shumod <absborodin@yandex.ru>
2015-10-26 18:04:56 +0300

    Начало темизации сайта

--- templates/articles.tpl.php
+++ templates/articles.tpl.php
@@ -1,6 +1,8 @@
-<div class="event">
-	<div class="image"><?php print $image;?></div>
-	<div class="created"><?php print $created;?></div>
-	<div class="title"><?php print $title;?></div>
-	<div class="text"><?php print $text;?></div>
+<div class="<?php print $entity_class;?>">
+	<div class="entity-image"><?php print $image;?></div>
+	<div class="entity-information">
+		<div class="created"><?php print $created;?></div>
+		<div class="title"><?php print $title;?></div>
+		<div class="text"><?php print $text;?></div>
+	</div>
 </div>
--- templates/places.tpl.php
+++ templates/places.tpl.php
@@ -1,5 +1,7 @@
-<div class="places">
-	<div class="place-image"><?php print $image;?></div>
-	<div class="place-title"><?php print $title;?></div>
-	<div class="place-description"><?php print $description;?></div>
+<div class="<?php print $entity_class;?>">
+	<div class="entity-image"><?php print $image;?></div>
+	<div class="entity-information">
+		<div class="entity-title"><?php print $title;?></div>
+		<div class="place-description"><?php print $description;?></div>
+	</div>
 </div>

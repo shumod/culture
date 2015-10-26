06c7d0bd340fc78ed584f11d97191ba1d1dbafd6
shumod <absborodin@yandex.ru>
2015-10-26 12:19:23 +0300

    Настроен раздел статей

---
+++ templates/articles.tpl.php
@@ -0,0 +1,6 @@
+<div class="event">
+	<div class="image"><?php print $image;?></div>
+	<div class="created"><?php print $created;?></div>
+	<div class="title"><?php print $title;?></div>
+	<div class="text"><?php print $text;?></div>
+</div>
